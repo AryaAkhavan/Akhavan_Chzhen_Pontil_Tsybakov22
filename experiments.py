37db1dbe5689c128329f7fb2b0e72e42e0ccf09d
aryaakhavan <aryaakhavan71@gmail.com>
2022-05-26 01:21:36 +0200

    Back to normal + Readme!

--- experiments.py
+++ experiments.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from joblib import Parallel, delayed
 
 
-from plotter import plot_results, plot_ratio
+from plotter import plot_results
 from black_box import BlackBox
 from oracles import ZeroOrderL1, ZeroOrderL2, ZeroOrderGaussian
 from objectives import FuncL2Test, FuncL1Test, NewTest, FTest, SumFuncL1Test
@@ -32,43 +32,6 @@ def inner_loop(i, estimator, sample, setup_optimizer,
             pickle.dump(report, fp)
     return report
 
-
-def multi_comp_SumFuncL1Test(criteria, dims,
-               norm_lipsch, norm_str_conv,
-               constr_type, sample,
-               max_iter, to_cache=True):
-
-    sign_style = [criteria, dims,
-                  norm_lipsch, norm_str_conv,
-                  constr_type, sample,
-                  max_iter]
-
-    SIGNATURE = ''.join(f'{value}_'.replace('.', 'DOT') for value in sign_style)
-
-    save_file_name = f'cache/{SIGNATURE}'
-    if to_cache and os.path.isfile(save_file_name):
-        with open(save_file_name, "rb") as fp:
-            results = pickle.load(fp)
-
-    else:
-        final_iter = {}
-        results = []
-        for dim in dims:
-            radius = np.log(dim) ** (1 / 2) if constr_type=='simplex' else 1
-            objective = SumFuncL1Test(dim=dim)
-            objective_min = objective.get_min()
-            res, sign = run_experiment(dim, max_iter, sample, constr_type,
-                                                radius, objective, norm_str_conv, norm_lipsch,
-                                                sigma, objective_min, noise_family, to_cache=True)
-            for idx, (method_name, res) in enumerate(res.items()):
-                error = np.array(res) - objective_min
-                mean = np.average(error, axis=0)
-                final_iter[method_name, dim] = mean[-1]
-
-            results.append(radius *final_iter['Our', dim] / final_iter['Spherical', dim])
-        with open(save_file_name, "wb") as fp:
-            pickle.dump(results, fp)
-    return results, SIGNATURE
 
 def run_loop(estimator, sample, setup_estimator, setup_optimizer,
              setup_black_box, SIGNATURE, to_cache):
@@ -152,47 +115,28 @@ def main(dim, max_iter, sample, constr_type, radius, objective,
 
 if __name__ == '__main__':
 
-    criteria = 'multi_dim'
-    constr_type = 'simplex'
 
-    if criteria == 'single_dim':
-        dim = 20000
-        radius = np.log(dim) ** (1 / 2) if constr_type == 'simplex' else 1
-        objective = SumFuncL1Test(dim=dim)
-        objective_min = objective.get_min()
-
-    if criteria == 'multi_dim':
-        dims = [1000, 2500, 5000, 7500, 10000, 15000, 20000]
-
-
-    max_iter = 1000000
+    dim = 1000
+    max_iter = 300000
     sample = 30
-
+    constr_type = 'simplex'
+    radius = np.log(dim)**(1/2) if constr_type=='simplex' else 1
+    objective = SumFuncL1Test(dim=dim)
     norm_str_conv = 1
     norm_lipsch = 1
     sigma = 0.
+    objective_min = objective.get_min()
     noise_family = 'Gaussian'
     to_plot = True
     to_cache = True
     to_save_plot = True
 
-
-    if criteria == 'single_dim':
-        results, SIGNATURE = main(dim, max_iter, sample, constr_type, radius,
-                               norm_str_conv, norm_lipsch, sigma,
-                               noise_family, to_cache,
+    results, SIGNATURE = main(dim, max_iter, sample, constr_type, radius,
+                              objective, norm_str_conv, norm_lipsch, sigma,
+                              objective_min, noise_family, to_cache,
                               to_save_plot)
 
 
-        if to_plot:
-            plot_results(max_iter, dim, constr_type,
+    if to_plot:
+        plot_results(max_iter, dim, constr_type,
                      objective_min, results, SIGNATURE, to_save_plot)
-
-
-    else:
-        results, SIGNATURE = multi_comp_SumFuncL1Test(criteria, dims,
-                         norm_lipsch, norm_str_conv,
-                            constr_type, sample,
-                           max_iter, to_cache=True)
-        if to_plot:
-            plot_ratio(dims, results, SIGNATURE, to_save_plot)
